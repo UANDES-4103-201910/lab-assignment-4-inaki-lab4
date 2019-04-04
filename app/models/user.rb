af8a271b54f7a8d7c9df4826d648e5e13bb37e7f
iepepay <ierrazuriz@miuandes.cl>
2019-04-04 11:06:51 -0300

    Ch-Ch-Ch-Ch-Changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :orders
+  validates :email, format: {with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "Invalid Email address"}
+  validates :phone_number, length: { in: 8..12}, numericaly: { only_integers: true}
+  validates :password, length: { in: 9..12}
 end
