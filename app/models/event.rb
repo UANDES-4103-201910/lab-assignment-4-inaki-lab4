7c4c03e3eb8bb082bc2648eec12f70f5a189ef61
iepepay <ierrazuriz@miuandes.cl>
2019-04-04 23:08:26 -0300

    smh

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,11 @@
 class Event < ApplicationRecord
   belongs_to :event_venue
   has_many :ticket_types
-  validate :start_date
+  validates :event_venue_id, uniqueness: {scope:start_date,
+                                            message: "There cannot be two or more events hosted in the same venue with the same start date"}
+  validate :start_date_not
+
+  def start_date_not
+    if start_date < Date.today
+      errors.add(:start_date, "Must be later than current date of creation")
 end
