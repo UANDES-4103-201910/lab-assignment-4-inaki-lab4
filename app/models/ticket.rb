8fc852858e4a92a6d2e964f68a837e61be352507
iepepay <ierrazuriz@miuandes.cl>
2019-04-04 23:59:56 -0300

    asd

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,16 @@
 class Ticket < ApplicationRecord
   belongs_to :ticket_type
   belongs_to :order
+  before_save :after_start_date
+  before_save :bought_after_start
+
+  private
+    def after_start_date
+      if create_at > start_date 
+
+    end
+    
+    def bought_after_start
+      
+    end  
 end
